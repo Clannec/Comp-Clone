6bc61b4c219ae64ba2b01f1144d6ef81b94302cf
Keith Rocheck <749812+krocheck@users.noreply.github.com>
2022-01-20 23:50:19 -0500

    minor: add variable parsing to textinput

--- webui/src/Components/TextInputField.jsx
+++ webui/src/Components/TextInputField.jsx
@@ -1,7 +1,12 @@
-import { useEffect, useMemo, useState, useCallback } from 'react'
+import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import { VariableDefinitionsContext } from '../util'
+import Tribute from 'tributejs'
 import { CInput } from '@coreui/react'
+import { decode } from 'html-entities'
 
 export function TextInputField({ definition, value, setValue, setValid }) {
+	const variableDefinitionsContext = useContext(VariableDefinitionsContext)
+
 	const [tmpValue, setTmpValue] = useState(null)
 
 	// Compile the regex (and cache)
@@ -51,18 +56,88 @@ export function TextInputField({ definition, value, setValue, setValid }) {
 		}
 	}, [isValueValid, definition.default, value, setValue, setValid])
 
+	const doOnChange = useCallback(
+		(e) => {
+			const newValue = decode(e.currentTarget.value, { scope: 'strict' })
+			setTmpValue(newValue)
+			setValue(newValue)
+			setValid?.(isValueValid(newValue))
+		},
+		[setValue, setValid, isValueValid]
+	)
+
+	const tribute = useMemo(() => {
+		// Create it once, then we attach and detach whenever the ref changes
+		return new Tribute({
+			values: [],
+			trigger: '$(',
+
+			// function called on select that returns the content to insert
+			selectTemplate: (item) => `$(${item.original.value})`,
+
+			// template for displaying item in menu
+			menuItemTemplate: (item) =>
+				`<span class="var-name">${item.original.value}</span><span class="var-label">${item.original.label}</span>`,
+		})
+	}, [])
+
+	useEffect(() => {
+		// Update the suggestions list in tribute whenever anything changes
+		const suggestions = []
+		for (const [instanceLabel, variables] of Object.entries(variableDefinitionsContext)) {
+			for (const va of variables) {
+				const variableId = `${instanceLabel}:${va.name}`
+				suggestions.push({
+					key: variableId + ')',
+					value: variableId,
+					label: va.label,
+				})
+			}
+		}
+
+		tribute.append(0, suggestions, true)
+	}, [variableDefinitionsContext, tribute])
+
+	const [, setupTributePrevious] = useState([null, null])
+	const setupTribute = useCallback(
+		(ref) => {
+			// we need to detach, so need to track the value manually
+			setupTributePrevious(([oldRef, oldDoOnChange]) => {
+				if (oldRef) {
+					tribute.detach(oldRef)
+					if (oldDoOnChange) {
+						oldRef.removeEventListener('tribute-replaced', oldDoOnChange)
+					}
+				}
+				if (ref) {
+					tribute.attach(ref)
+					ref.addEventListener('tribute-replaced', doOnChange)
+				}
+				return [ref, doOnChange]
+			})
+		},
+		[tribute, doOnChange]
+	)
+
 	// Render the input
-	return (
+	return definition.variableLookup === true && !regex ? (
+		<CInput
+			type="text"
+			innerRef={setupTribute}
+			value={tmpValue ?? value ?? ''}
+			style={{ color: !isValueValid(tmpValue ?? value) ? 'red' : undefined }}
+			title={definition.tooltip}
+			onChange={doOnChange}
+			onFocus={() => setTmpValue(value ?? '')}
+			onBlur={() => setTmpValue(null)}
+		/>
+	) : (
 		<CInput
 			type="text"
 			value={tmpValue ?? value ?? ''}
 			style={{ color: !isValueValid(tmpValue ?? value) ? 'red' : undefined }}
 			title={definition.tooltip}
-			onChange={(e) => {
-				setTmpValue(e.currentTarget.value)
-				setValue(e.currentTarget.value)
-				setValid?.(isValueValid(e.currentTarget.value))
-			}}
+			onChange={doOnChange}
 			onFocus={() => setTmpValue(value ?? '')}
 			onBlur={() => setTmpValue(null)}
 		/>
